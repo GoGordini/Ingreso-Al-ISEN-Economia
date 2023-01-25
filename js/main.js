3ff7518f90fb2f9b3ee749a7c8f5cc38270e605b
GoGordini <eleonora.tubio@gmail.com>
2023-01-25 14:57:15 -0300

    JS ahora sí

---
+++ js/main.js
@@ -0,0 +1,13 @@
+let nombre;
+nombre="Gordini";
+console.log(nombre);
+nombre="Puto El Que Lee"
+console.log(nombre);
+let mensaje="Mi nombre es " + nombre;
+console.log(mensaje);
+let tuNombre=prompt ("Ingrese su nombre");
+console.log(tuNombre,typeof tuNombre);
+let tuEdad=parseInt(prompt ("Ingrese su edad"));
+console.log(tuEdad);
+alert("Puto el que lee");
+
